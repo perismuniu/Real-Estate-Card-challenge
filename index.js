e533c825dd0402825b50e2305645e49451608381
Peris Muniu <122822923+perismuniu@users.noreply.github.com>
2024-10-05 12:57:05 +0300

    challenge completed

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-function getPropertyHtml() {
-/*
-SUPER CHALLENGE 💪
+import  { propertyForSaleArr }  from './properties/propertyForSaleArr.js'
+import { placeholderPropertyObj } from './properties/placeholderPropertyObj.js'
 
-Render out a card for each of the properties in the propertyForSaleArr array (in the 'properties' folder). Each card should have an image, a property location, a price, a comment and the TOTAL property size in square metres (each object has an array with the size in square metres of the individual rooms).
+// Function to generate HTML for properties
+function getPropertyHtml(propertyForSaleArr = [placeholderPropertyObj]) {
 
-If no array of properties is passed to getPropertyHtml, the placeholder property stored in placeholderPropertyObj (in the 'properties' folder) should be rendered instead.
+    // Using .map() to iterate over the array of properties and destructure necessary fields
+    return propertyForSaleArr.map(({ propertyLocation, priceGBP, roomsM2, comment, image }) => {
+        
+        // Calculating the total square meters by summing up all values in the roomsM2 array using .reduce()
+        const total= roomsM2.reduce((total, property)=>{
+            return total + property
+        }, 0)
+    
+        // Returning the HTML structure for each property card with interpolated values
+        return  `<section class="card">
+                    <img src="/images/${image}" alt="${propertyLocation}">
+                    <div class="card-right">
+                        <h2>${propertyLocation}</h2>
+                        <h3>${priceGBP}</h3>
+                        <p>${comment}</p>
+                        <h3>${total} m&sup2;</h3>
+                    </div>
+                </section> 
+                `
+    })
 
-This is the JS I want you to use to complete this challenge 👇
-- import/export
-- .map()
-- .join()
-- Object destructuring
-- .reduce()
-- Default parameters
-
-The HTML and CSS have been done for you. 
-This is the HTML template 👇. Replace everything in UPPERCASE with property data.
-
-<section class="card">
-    <img src="/images/IMAGE">
-    <div class="card-right">
-        <h2>PROPERTY LOCATION</h2>
-        <h3>PRICE GBP</h3>
-        <p>COMMENT</p>
-        <h3>TOTAL SIZE IN SQUARE METRES m&sup2;</h3>
-    </div>
-</section> 
-*/
 }
 
-/***** Modify 👇 by adding an argument to the function call ONLY. *****/
-document.getElementById('container').innerHTML = getPropertyHtml()
+// Inserting the generated HTML into the element with id 'container' and joining array elements into a single string
+document.getElementById('container').innerHTML = getPropertyHtml(propertyForSaleArr ).join(' ')
--- properties/propertyForSaleArr.js
+++ properties/propertyForSaleArr.js
@@ -1,4 +1,4 @@
-const propertyForSaleArr = [
+export const propertyForSaleArr = [
     {
         propertyLocation: 'Kensington, London',
         priceGBP: 890000,
@@ -28,5 +28,3 @@ const propertyForSaleArr = [
         image: 'shed.jpg'
     }
 ]
-
-
